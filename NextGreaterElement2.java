869dfdc4a6650bfd20e9ca695bbb87c5eaeed24b
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-09-30 23:22:32 +0530

    Create NextGreaterElement2.java

---
+++ NextGreaterElement2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n= nums.length;
+        int []ans = new int[n];
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=2*n-1;i>=0;i--){
+
+            while(!s.isEmpty() && nums[i%n]>=s.peek()){
+                s.pop();
+            }
+
+            if(i<n){
+                if(!s.isEmpty()){
+                ans[i] = s.peek();
+                }
+            else{
+                ans[i] = -1;
+            }
+        }
+
+        s.push(nums[i%n]);
+        
+    }
+    
+    return ans;
+    }
+}
