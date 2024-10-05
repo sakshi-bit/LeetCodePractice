552950bfa680b638b81da245e4224fb6d100ec8a
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-10-06 00:19:39 +0530

    Update TrappingRainWater.java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -29,3 +29,34 @@ class Solution {
         return water;
     }
 }
+
+Approach 2 : Monotonic Stack
+O(n)
+O(n)
+
+    class Solution {
+    public int trap(int[] height) {
+
+        int n = height.length;
+        int water = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int right=0;right<n;right++){
+            while(!stack.isEmpty() && height[stack.peek()] < height[right]){
+
+                int mid = stack.pop();
+
+                if(stack.isEmpty()){
+                    break;
+                }
+                int left = stack.peek();
+                int minHeight = Math.min((height[right]-height[mid]),height[left]-height[mid]);
+                int width = right-left-1;
+                water+=minHeight*width;
+            }
+            stack.push(right);
+        }
+        return water;   
+    }
+}
