a927b9db198ba73d230c1d3d5ec109a9bebd5850
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-11-17 23:10:28 +0530

    Update longest_substring_without_repeating_characters.cpp

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -1,29 +1,34 @@
-#include <string>
-#include <unordered_map>
-using namespace std;
-int getMaximumSubstringUnique(string str)
-{
-	unordered_map<char, int>mpp;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
 
-	int i = 0, j = 0;
-	int n = str.length();
-	int mx = INT_MIN;
-	while (j < n) {
-		mpp[str[j]]++;
-		if (mpp.size() == j-i+1) {
-			mx = max(mx, j - i + 1);
-			j++;
-		}
-		else if (mpp.size() < j - i + 1) {
-			while (mpp.size() < j - i + 1) {
-				mpp[str[i]]--;
-				if (mpp[str[i]] == 0) {
-					mpp.erase(str[i]);
-				}
-				i++;
-			}
-		}
-		j++;
-	}
-	return mx;
-}
+        unordered_map<char,int>mpp;
+        int i=0,j=0;
+        int n = s.length();
+
+        int maxi = INT_MIN;
+        if(s.length() == 0){
+            return 0;
+        }
+        while(j<n){
+            mpp[s[j]]++;
+
+            if(mpp.size() == j-i+1){
+                maxi = max(maxi,j-i+1);
+                j++;
+            }else if(mpp.size() < j-i+1){
+                while(mpp.size()<j-i+1){
+                mpp[s[i]]--;
+                if(mpp[s[i]] == 0){
+                    mpp.erase(s[i]);
+                }
+                i++;
+            }
+            j++;
+            }
+            
+            
+        }
+        return maxi;
+    }
+};
