5099d2999f81010ceaef99da26ed94c112177c2b
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-11-21 23:19:40 +0530

    Create NextSmallerToRight.cpp

---
+++ NextSmallerToRight.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+vector<int> NSR(vector<int> arr, int n)
+{
+	vector<int>v;
+	stack<int>s;
+	for (int i = n-1; i >= 0; i--) {
+		while (s.size() > 0 && s.top() >= arr[i]) {
+			s.pop();
+		}
+		if (s.size() == 0) {
+			v.push_back(-1);
+		}
+		else {
+			v.push_back(s.top());
+		}
+		s.push(arr[i]);
+	}
+	reverse(v.begin(), v.end());
+	return v;
+}
