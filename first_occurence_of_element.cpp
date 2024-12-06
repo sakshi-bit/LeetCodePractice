ad786bf8a41702c9ad5865e90cfd5d38a37a1485
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-12-06 23:21:32 +0530

    Create first_occurence_of_element.cpp

---
+++ first_occurence_of_element.cpp
@@ -0,0 +1,21 @@
+#include "first_occurence_of_an_element.h"
+int firstOccurenceOfElement(vector<int> arr, int n, int elem)
+{
+	int start = 0;
+	int end = n - 1;
+	int res = -1;
+	while (start <= end) {
+		int mid = start + (end - start) / 2;
+		if (elem == arr[mid]) {
+			res = mid;
+			end = mid - 1;
+		}
+		else if (elem < arr[mid]) {
+			end = mid - 1;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+	return res;
+}
