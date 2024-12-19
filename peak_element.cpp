bb686d07aa5eabef0b999ccc9e253cc0650070e3
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2024-12-19 23:34:39 +0530

    Create peak_element.cpp

---
+++ peak_element.cpp
@@ -0,0 +1,37 @@
+#include "peak_element.h"
+
+int findPeakElement(vector<int> arr, int n)
+{
+	int low = 0, high = n - 1;
+	while (low <= high) {
+		int mid = low + (high - low) / 2;
+		if (mid > 0 && mid < n - 1) {
+			if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+				return mid;
+			}
+			else if (arr[mid - 1] > arr[mid]) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+		else if (mid == 0) {
+			if (arr[0] > arr[1]) {
+				return 0;
+			}
+			else {
+				return 1;
+			}
+		}
+		else if (mid == n - 1) {
+			if (arr[n - 1] > arr[n - 2]) {
+				return n - 1;
+			}
+			else {
+				return n - 2;
+			}
+		}
+	}
+	return -1;
+}
