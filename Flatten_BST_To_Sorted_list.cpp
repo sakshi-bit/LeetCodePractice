4fc620fb0a9da776858b799ecef3e2397af831ee
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2025-04-13 21:40:07 +0530

    Update Flatten_BST_To_Sorted_list.cpp

--- Flatten_BST_To_Sorted_list.cpp
+++ Flatten_BST_To_Sorted_list.cpp
@@ -1,3 +1,5 @@
+TC=  O(n^2) 
+
    Node *flattenBST(Node *root)
     {
         if(root == NULL){
@@ -17,6 +19,34 @@
             temp->right=root;
         }else{
             head=root;
+        }
+        return head;
+    }
+
+TC= O(n)
+
+class Solution
+{
+public:
+    Node* prev = NULL;
+    Node *flattenBST(Node *root)
+    {
+        if(root == NULL){
+            return NULL;
+        }
+        
+        Node* head = flattenBST(root->left);
+        root->left=NULL;
+        if(prev!=NULL){
+            prev->right=root;
+            prev=root;
+        }else{
+            prev=root;
+        }
+        
+        root->right = flattenBST(root->right);
+        if(head==NULL){
+            return root;
         }
         return head;
     }
